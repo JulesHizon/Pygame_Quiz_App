50a85250239068cb0abbe8b7fff8196e0cfadbf7
Jules Hizon <hizonjules@gmail.com>
2022-02-28 22:34:36 -0800

    updated logic to interact with ui

--- question_logic.py
+++ question_logic.py
@@ -7,9 +7,7 @@ class QuestionLogic():
 
     def next_question(self):
         prompt = f"{self.current_question + 1}. True or False: {self.qb.question_bank[self.current_question]['question']}"
-        print(prompt)
-        player_answer = input("Please write 'True' or 'False': ")
-        return self.is_valid_answer(player_answer)
+        return prompt
 
     def check_answer(self, answer):
         if answer == self.qb.question_bank[self.current_question]['correct_answer']:
